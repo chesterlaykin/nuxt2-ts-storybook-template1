02c8a252698bcb03b7c270e14d4b3a35423248b9
Joel <joelsetterberg@hotmail.com>
2022-08-28 23:04:54 +0200

    add dependencies, config things, sample component, sass folders

---
+++ pages/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <Task  :task="{ id: '4', state: 'TASK_INBOX', title: 'First task' }"/>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  name: 'IndexPage'
+})
+</script>
+<style lang="scss" scoped>
+  .page {
+      background: #2cc5d2;
+  }
+  </style>
